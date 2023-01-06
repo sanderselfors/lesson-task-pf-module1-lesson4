3a22a7159d54c4f488a8901e9430457b192eb3fe
sanderselfors <sanost93689@stud.noroff.no>
2023-01-06 11:17:16 +0100

    lesson task 4

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Question 1 Write a loop that logs the numbers 0 to 7.
+
+for (var i = 0; i <= 7; i++) {
+  console.log(i);
+}
+
+// Question 2 Write a loop that logs the numbers 10 to 1. Count down from 10 to 1.
+
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+  }
+
+// Question 3 Write a loop that counts from 10 to 20, and log only the odd numbers. 11, 13, 15, etc
+
+for (var a = 11; a <= 20; a+=2) {
+    console.log(a);
+  }
